f3ae48d900916b7b75fb552377f4691e6f4e7d58
shidomnt <hatranquang02@gmail.com>
2021-05-01 23:43:07 +0700

    mang2c

--- LuyenCode/Mang_2_chieu/Baitaptonghop.cpp
+++ LuyenCode/Mang_2_chieu/Baitaptonghop.cpp
@@ -85,6 +85,55 @@ int GTNNch3(int a[][100],int m, int n){                //Tim gia tri nho nhat %3
     if(min%3!=0) return 0;
     else return min;
 }
+void xoacot(int a[][100],int m,int &n,int pos){         //Ham xoa cot
+    for(int i=0;i<m;i++){
+        for(int j=pos-1;j<n;j++)
+        a[i][j]=a[i][j+1];
+    }
+    n--;
+}
+void xoahang(int a[][100],int &m,int n,int pos){         //Ham xoa hang
+    for(int i=0;i<n;i++){
+        for(int j=pos-1;j<m;j++)
+        a[j][i]=a[j+1][i];
+    }
+    m--;
+}
+void swap(int &a,int &b){                               //Ham doi vi tri 2 phan tu
+    a=a^b;
+    b=a^b;
+    a=a^b;
+}
+void swaphang(int a[][100],int m,int n,int p1,int p2){  //Ham doi vi tri 2 hang
+    for(int i=0;i<n;i++){
+        swap(a[p1-1][i],a[p2-1][i]);
+    }
+}
+void swapcot(int a[][100],int m,int n,int p1,int p2){   //Ham doi vi tri 2 cot
+    for(int i=0;i<m;i++){
+        swap(a[i][p1-1],a[i][p2-1]);
+    }
+}
+int GTLNdcc(int a[][100],int m,int n){
+    int i=1,j=1;
+    int max=a[0][0];
+    while(i<m&&j<n){
+        if(max<a[i][j]) max=a[i][j];
+        i++;
+        j++;
+    }
+    return max;
+}
+int GTNNdcc(int a[][100],int m,int n){
+    int i=1,j=1;
+    int min=a[0][0];
+    while(i<m&&j<n){
+        if(min>a[i][j]) min=a[i][j];
+        i++;
+        j++;
+    }
+    return min;
+}
 int main(){
     int a[100][100];
     int b[100][100];
@@ -96,10 +145,7 @@ int main(){
     scanf("%d",&n);
     printf("Nhap ma tran A:\n");
     nhapmatran(a,m,n);
-    /*
-    printf("Nhap ma tran B:\n");
-    nhapmatran(b,m,n);
-    */
+//Tong phan tu tai hang chan/GTLN cua 1 cot
     /*
     xuatmatran(a,m,n);  
     printf("Tong cac phan tu tren hang chan: %d\n",tongpthangchan(a,m,n));
@@ -108,23 +154,65 @@ int main(){
     scanf("%d",&cot);
     printf("Gia tri lon nhat cua cot %d la: %d",cot,GTLNcotN(a,m,n,cot));
     */
+//GTLN/GTNN cua ma tran
    /*
     printf("GTLN= %d\n",GTLN(a,m,n));
     printf("GTNN= %d\n",GTNN(a,m,n));
     */
+//Tong hai ma tran
     /*
+    printf("Nhap ma tran B:\n");
+    nhapmatran(b,m,n);
     tong2matran(a,b,c,m,n);
     printf("Tong 2 ma tran a,b la: \n");
     xuatmatran(c,m,n);
     */
+//Sap xep ma tran theo hang/Tong cac phan tu/GTNN chia het cho 3
    /*
     sapxephang(a,m,n);
     printf("Sau khi sap xep: \n");
     xuatmatran(a,m,n);
     printf("Tong cac phan tu: %3d\n",tongpt(a,m,n));
     int GTNNc3=GTNNch3(a,m,n);
-    if(!GTNNc3) printf("Khong co!\n");
+    if(!GTNNc3) printf("Khong co gia tri nao chia het cho 3!\n");
     else printf("GTNN chia het cho 3 la: %3d\n",GTNNc3);
+    */
+//Xoa cot
+   /*
+    printf("Nhap cot muon xoa: \n");
+    int xcot;
+    scanf("%d",&xcot);
+    xoacot(a,m,n,xcot);
+    printf("Mang sau khi xoa: \n");
+    xuatmatran(a,m,n);
+    */
+//Xoa hang
+    /*
+    printf("Nhap hang muon xoa: \n");
+    int xhang;
+    scanf("%d",&xhang);
+    xoahang(a,m,n,xhang);
+    printf("Mang sau khi xoa: \n");
+    xuatmatran(a,m,n);
+    */
+//Doi cho 2 hang/cot
+    /*
+    int p1,p2;
+    printf("Nhap lan luot 2 hang muon doi: \n");
+    scanf("%d%d",&p1,&p2);
+    swaphang(a,m,n,p1,p2);
+    printf("Mang sau khi doi: \n");
+    xuatmatran(a,m,n);
+    printf("Nhap lan luot 2 cot muon doi: \n");
+    scanf("%d%d",&p1,&p2);
+    swapcot(a,m,n,p1,p2);
+    printf("Mang sau khi doi: \n");
+    xuatmatran(a,m,n);
+    */
+//GTLN/GTNN tren duong cheo chinh
+/*
+    printf("GTLN tren duong cheo chinh la: %3d\n",GTLNdcc(a,m,n));
+    printf("GTNN tren duong cheo chinh la: %3d\n",GTNNdcc(a,m,n));
     */
     return 0;
 }
