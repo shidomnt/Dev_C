7759c3112f83cdb6e9e14f86983c3efc8008707e
shidomnt <hatranquang02@gmail.com>
2021-05-04 12:38:55 +0700

    d

--- LuyenCode/nhap4.cpp
+++ LuyenCode/nhap4.cpp
@@ -1,9 +1,16 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+ 
 int main(){
-    char string[100];
-    char string2[100];
-    scanf("%[a-z A-Z 0-9]",string);
-    printf("%s\b\b\b",string);
-    return 0;
+    // Khai báo mảng có 5 phần tử
+    int arr[] = {1, 2, 3, 4, 5};
+ 
+    printf("Dia chi cua mang arr = %d\n", &arr);
+    printf("Gia chi cua mang arr = %d\n", arr);
+    // Thử in địa chỉ của từng phần tử
+    // sizeof (arr): Kích thước của mảng
+    // sizeof (int): Kích thước của kiểu int
+    for(int i = 0; i < sizeof (arr) / sizeof (int); i++){
+        printf("Dia chi cua arr[%d] = %d\n", i, &arr[i]);
+    }
+ 
 }
