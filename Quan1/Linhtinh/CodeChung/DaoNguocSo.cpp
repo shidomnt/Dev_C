c323edd4f574529178bb760708b52ced5428672c
shidomnt <hatranquang02@gmail.com>
2021-05-04 23:25:14 +0700

    m

--- Quan1/Linhtinh/CodeChung/DaoNguocSo.cpp
+++ Quan1/Linhtinh/CodeChung/DaoNguocSo.cpp
@@ -1,34 +1,20 @@
 #include<stdio.h>
 #include<math.h>
-#include<stdbool.h>
-void sapxep(int a[],int n){
-    for(int i=0;i<n;i++){
-        int temp=a[i];
-        int j=i-1;
-        while(j>=0&&temp<a[j]){
-            a[j+1]=a[j];
-            j--;
-        }
-        a[j+1]=temp;
-    }
-}
-void nhapmang(int v[],int n){
-    for(int i=0;i<n;i++)
-    scanf("%d",&v[i]);
-}
-int maxbanhchung(int v[],int n,int V){
-    int i,s=0;
-    for(i=n;i>=0;i--){
-        if(s+v[i]<=V) s+=v[i];
-    }
-    return s;
-}
+#include<stdbool.h> 
 int main(){
-    int n,V;
-    scanf("%d%d",&n,&V);
-    int v[n];
-    nhapmang(v,n);
-    sapxep(v,n);
-    printf("%d",maxbanhchung(v,n,V));
+    double a1,b1,c1,a2,b2,c2;
+    scanf("%lf%lf%lf%lf%lf%lf",&a1,&b1,&c1,&a2,&b2,&c2);
+    double D,Dx,Dy;
+    D=a1*b2-b1*a2;
+    Dx=c1*b2-b1*c2;
+    Dy=a1*c2-c1*a2;
+    if(D==0){
+        if(Dx+Dy==0){
+            printf("VOSONGHIEM");
+        }
+        else
+        printf("VONGHIEM");
+    }
+    else printf("%.2lf %.2lf",Dx/D,Dy/D);
     return 0;
 }
