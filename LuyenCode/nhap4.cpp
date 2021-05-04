acdb578e4d860de75821ea050da9405b5995401f
shidomnt <hatranquang02@gmail.com>
2021-05-04 09:50:35 +0700

    xoa exe

--- LuyenCode/nhap4.cpp
+++ LuyenCode/nhap4.cpp
@@ -2,7 +2,8 @@
 #include<string.h>
 int main(){
     char string[100];
-    scanf("%[\n]",string);
-    printf("%s\n",string);
+    char string2[100];
+    scanf("%[a-z A-Z 0-9]",string);
+    printf("%s\b\b\b",string);
     return 0;
 }
