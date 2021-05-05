a17625174c1c8370cd75a84cfb208399d98373d4
shidomnt <hatranquang02@gmail.com>
2021-05-05 19:09:11 +0700

    m

--- LuyenCode/nhap4.cpp
+++ LuyenCode/nhap4.cpp
@@ -1,23 +1,34 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
 #include <stdlib.h>
-long long int giaithua(int n){
-    if (n == 1)
-        return 1;
-    return n * giaithua(n - 1);
-}
-long long int tohopchap2(int n){
-    long long int ts=giaithua(n);
-    long long int ms=giaithua(n-2)*2;
-    return ts/ms;
-}
-int C(int k, int n) {
-    if (k == 0 || k == n) return 1;
-    if (k == 1) return n;
-    return C(k - 1, n - 1) + C(k, n - 1);
-}
-int main()
-{
-   int a=67;
-   printf("%lli",C(2,a));
-   return(0);
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    char s[1000] = "HackerRank ";
+    int a;
+    double b;
+    char string[100];
+    scanf("%d",&a);
+    scanf("%lf",&b);
+    fflush(stdin);
+    scanf("%[^\n]",string);
+    strcat(s,string);
+    printf("%d\n",a+i);
+    printf("%.1lf\n",b+d);
+    puts(s);
+    
+    // Declare second integer, double, and String variables.
+    
+    // Read and save an integer, double, and String to your variables.
+    
+    // Print the sum of both integer variables on a new line.
+    
+    // Print the sum of the double variables on a new line.
+    
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+
+    return 0;
 }
