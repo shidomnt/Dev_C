643e44da684d1740b319d03208cc1f7d2a28b67b
shidomnt <hatranquang02@gmail.com>
2021-05-08 20:09:49 +0700

    m

--- LuyenCode/nhap4.cpp
+++ LuyenCode/nhap4.cpp
@@ -1,14 +1,26 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
-
-int main() {
-    printf("Nhap ma sinh vien: ");
-    char matruong[5];
-    char makhoa[10];
-    scanf("%[a-zA-Z]s",&matruong);
-    scanf("%",makhoa);
-    printf("\n%s\n%s",matruong,makhoa);
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int space_count(char *string, int *a,int *n){
+    if(strchr(string,' ')==NULL)
     return 0;
+    else{
+        *(a+(*n))= (int)strchr(string,' ');
+        (*n)++;
+        return 1 + space_count(strchr(string,' ') + 1 , a, n);
+    }
+}
+int main(){
+    char *string;
+    string = (char*) malloc(100*sizeof(char));
+    //Nhap chuoi
+    scanf("%[^\n]",string);
+    int *a = (int*) malloc(100*sizeof(int));
+    int n=0;
+    int sl_space = space_count(string,a,&n);
+    for(int i=0;i<=n;i++)
+    printf("%d ",a[i])
+    
+    return 0;
+    
 }
