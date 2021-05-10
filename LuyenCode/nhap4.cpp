c2f2a19fed30ecc7399e4c69c74769b1167b1cb3
shidomnt <hatranquang02@gmail.com>
2021-05-10 16:12:51 +0700

    m

--- LuyenCode/nhap4.cpp
+++ LuyenCode/nhap4.cpp
@@ -1,85 +1,36 @@
 #include<stdio.h>
 #include<math.h>
-void nhap(long long a[],int n){
-    for(int i=0;i<n;i++)
-        scanf("%lli",&a[i]);
-}
-void sapxep(long long a[],int n){
-    for(int i=0;i<n;i++){
-        int j=i-1;
-        long long temp=a[j+1];
-        while(j>=0&&temp>a[j]){
-            a[j+1]=a[j];
-            j--;
-        }
-        a[j+1]=temp;
-    }
-}
-void dupfilter(long long a[],int *n){
-    for(int i=0;i<(*n);i++){
-        while(a[i]==a[i+1]){
-            for(int j=i;j<(*n);j++){
-                a[j]=a[j+1];
-            }
-            (*n)--;
-        }
-    }
-}
-void counting_sort(long long a[],int n){
-    long long max=a[0];
-    long long min=a[0];
-    for(int i=0;i<n;i++){
-        if(max<a[i])
-        max=a[i];
-        else if(min>a[i])
-        min=a[i];
-    }
-    int k=max-min+1;
-    int count[k];
-    for(int i=0;i<k;i++)
-    count[i]=0;
-    for(int i=0;i<n;i++){
-        count[a[i]-min]++;
-    }
-    for(int i=1;i<k;i++){
-        count[i]+=count[i-1];
-    }
-    long long b[n];
-    for(int i=0;i<n;i++){
-        b[count[a[i]-min]-1] = a[i];
-        count[a[i]-min]--;
-    }
-    for(int i=0;i<n;i++){
-        a[i]=b[i];
-    }
+#include<stdbool.h>
+bool kiemtra(long n){
+    for(int i=2;i<=sqrt(n);i++)
+        if(n%i==0)
+            return false;
+    return true;
 }
 int main(){
     int n;
     scanf("%d",&n);
-    long long a[n];
-    nhap(a,n);
-    counting_sort(a,n);
-    int dem=1;
-    int b[n];
-    int k=0;
-    for(int i=0;i<n;i++){
-        if(a[i]==a[i+1]){
+    long a[n][2];
+    long max=0;
+    for(int i=0;i<n;i++)
+        for(int j=0;j<2;j++){
+            scanf("%li",&a[i][j]);
+            if(a[i][j]>max&&j==1) max = a[i][j];
+        }
+    long b[max];
+    int i;
+    int dem=0;
+    for(i=2;i<=max;i++){
+        if(kiemtra(i)){
+            b[dem]=i;
             dem++;
         }
-        else{
-            b[k]=dem;
-            dem=1;
-            k++;
-        }
     }
-    dupfilter(a,&n);
-    int max=b[0];
-    int pos=0;
-    int i;
-    for(i=0;i<k;i++)
-        if(max<b[i]){
-            max=b[i];
-            pos=i;
-        }
-    printf("%d %d",a[pos],b[pos]);
+    for(int j=0;j<n;j++){
+        int pos1=0,pos2=dem;
+        while(b[pos1]<a[j][0]) pos1++;
+        while(b[pos2]>a[j][1]) pos2--;
+        printf("%d\n",pos2-pos1+1);
+    }
+    
 }
