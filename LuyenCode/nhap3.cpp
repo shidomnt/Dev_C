acaec05d4b61a0ac465560a36d0dd8cd4b5a6007
shidomnt <hatranquang02@gmail.com>
2021-05-12 13:43:57 +0700

    m

--- LuyenCode/nhap3.cpp
+++ LuyenCode/nhap3.cpp
@@ -4,8 +4,21 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <stdbool.h>
-
+void sapxep(char *string){
+    int length=strlen(string);
+    for(int i=0;i<length-1;i++)
+        for(int j=i+1;j<length;j++){
+            if(string[j]<string[i]){
+                char temp=string[j];
+                string[j]=string[i];
+                string[i]=temp;
+            }
+        }
+}
 int main() {
-    printf("%d",-5%30);
+    char a[30];
+    gets(a);
+    sapxep(a);
+    puts(a);
     return 0;
 }
